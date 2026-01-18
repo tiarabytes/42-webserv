4cc866a7c34ea683667561250774eb4bf215bd06
Tiara <64247886+tiarabytes@users.noreply.github.com>
2026-01-18 19:24:50 +0800

    resubmit

---
+++ webserver/src/main.cpp
@@ -0,0 +1,15 @@
+#include "WebServer.hpp"
+
+int main(int argc, char **argv) {
+
+    if (argc != 2) {
+        std::cerr << "Usage: " << argv[0] << " <config_file>" << std::endl;
+        return 1;
+    }
+    WebServer webserver(argv[1]);
+    if (!webserver.correctConfig())
+        return 1;
+
+    webserver.initService();
+    return 0;
+}
